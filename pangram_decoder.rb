40e60eb6e399e65409bd2a2435a1eb99c57b3f0b
Jeremiah Munson <j.munson225@gmail.com>
2018-11-01 10:52:53 -0500

    added comments

--- pangram_decoder.rb
+++ pangram_decoder.rb
@@ -1,3 +1,4 @@
+# Reading in the list of pangrams stored in pangrams.txt
 file = File.new("pangrams.txt", "r")
 list = Array.new()
 while (line = file.gets)
@@ -5,23 +6,25 @@ while (line = file.gets)
 end
 file.close
 
-valid = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-
+# Want pangrams to be stored in this hash table where the number of characters is the key
+# The value is an array of pangrams with the same length
 pangrams = Hash.new()
 
+# This next part strips away all the unwanted characters (white space, commas, quotes, dashes, etc.)
+validCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 for line in list
     newLine = ""
     numberChars = 0
     line.each_char do |character|
-        for validChar in valid
-            if (character == validChar) 
+        for valid in validCharacters
+            if (character == valid) 
                 newLine+=character
                 numberChars+=1
                 break
             end
         end
     end
+    # This is an if statement, if key in use append, otherwise make new key/value
     pangrams.key?(numberChars) ? pangrams[numberChars].push(newLine) : pangrams[numberChars] = [newLine] 
 end
 
-puts pangrams[26]
