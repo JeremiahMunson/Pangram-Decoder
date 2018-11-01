7c86aabf42b663658127b7b4dfdb2b9a69198f2a
Jeremiah Munson <j.munson225@gmail.com>
2018-11-01 10:41:38 -0500

    adds pangrams-spaces to hash, length is key: list pangrams value

--- pangram_decoder.rb
+++ pangram_decoder.rb
@@ -7,19 +7,21 @@ file.close
 
 valid = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-puts list[0]
+pangrams = Hash.new()
 
 for line in list
     newLine = ""
+    numberChars = 0
     line.each_char do |character|
         for validChar in valid
             if (character == validChar) 
-                newLine.push(character) 
+                newLine+=character
+                numberChars+=1
                 break
             end
         end
     end
-    line = newLine
+    pangrams.key?(numberChars) ? pangrams[numberChars].push(newLine) : pangrams[numberChars] = [newLine] 
 end
 
-puts list[0]
+puts pangrams[26]
